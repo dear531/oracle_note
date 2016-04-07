149d4d5fcc2b1ece3e1817f20d02d76e3bafc83f
zhangly <dear531@21cn.com>
2016-04-07 09:15:28 +0800

    add script for search case

---
+++ search_case.sql
@@ -0,0 +1,17 @@
+declare
+	salary integer := 5;
+begin
+	case
+	when salary >= 1 and salary <= 2
+	then
+		dbms_output.put_line('[1,2]');
+	when salary > 2 and salary <= 3
+	then
+		dbms_output.put_line('(2,3]');
+	when salary > 3 and salary <= 4
+	then
+		dbms_output.put_line('(3,4]');
+	else
+		dbms_output.put_line('isn''t match');
+	end case;
+end;
