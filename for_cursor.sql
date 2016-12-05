53d36124f0e74b5db1a3dd5faffc84669e408f67
zhangly <dear531@21cn.com>
2016-12-05 14:53:08 +0800

    optimized for cursor

--- for_cursor.sql
+++ for_cursor.sql
@@ -1,6 +1,8 @@
 declare
+	cursor c_job is select * from t1;
 begin
-	for c_row in (select * from t1)
+	for c_row in c_job
+	--for c_row in (select * from t1)
 	loop
 		dbms_output.put_line('g1_id:'||c_row.g1_id||', g1_name:'||c_row.g1_name);
 	end loop;
