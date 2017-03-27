f5afc0501f8449affc124aaff254265cc44bf3bd
zhangliuying <dear531@21cn.com>
2017-03-27 14:44:40 +0800

    usage of type

---
+++ type.sql
@@ -0,0 +1,11 @@
+declare
+	var_ename emp.ename%type;
+	var_job emp.job%type;
+begin
+	select ename, job
+	into var_ename, var_job
+	from emp
+	where empno = 7369;
+	dbms_output.put_line(var_ename||'的职务是'||var_job);
+	end;
+/
