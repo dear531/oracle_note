28800cb8860c84b3aa4a12eb748616776c380413
zhangliuying <dear531@21cn.com>
2017-03-27 16:05:42 +0800

    usage of rowtype

---
+++ rowtype.sql
@@ -0,0 +1,9 @@
+declare
+	rowVar_emp emp%rowtype;
+begin
+	select * into rowVar_emp
+	from emp
+	where empno = 7369;
+	dbms_output.put_line('雇员'||rowVar_emp.ename||'的编号'||rowVar_emp.empno||'，职务是'||rowVar_emp.job);
+	end;
+/
