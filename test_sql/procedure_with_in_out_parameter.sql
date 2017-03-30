a5f19b673e9b219b44a458f33d1ecd5e30e9e5d9
zhangliuying <dear531@21cn.com>
2017-03-30 16:52:54 +0800

    optimized procedure in & out procedure

--- test_sql/procedure_with_in_out_parameter.sql
+++ test_sql/procedure_with_in_out_parameter.sql
@@ -11,18 +11,16 @@ begin
                    dbms_output.put_line('部门编号不存在');
 end select_dept;
 /*
-调用方法及结果输出：
-SQL> l  
+SQL> r   
   1  declare
-  2     dname varchar2(14);
-  3     loc varchar2(13);
+  2     dname dept.dname%type;
+  3     loc dept.dname%type;
   4  begin
   5     select_dept(81, dname, loc);
   6     if sql%found then
   7             dbms_output.put_line('dname:'||dname||',loc:'||loc);
   8     end if;
   9* end;
-SQL> /
 dname:81name,loc:81loc
 
 PL/SQL procedure successfully completed.
